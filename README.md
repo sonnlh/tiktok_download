dad8647eca2af46eb8443e89bd7f586a7949af08
sean <sonnlh57@gmail.com>
2025-01-08 22:14:03 +0700

    upload project

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 a very simple web version, to download the video in the best quality.
 
 ### how to use the app locally?
-- `git clone https://github.com/zalazarc20/tiktokDL.git`
+- `git clone https://https://github.com/sonnlh/tiktok_download.git`
 - `npm install`
 - once the modules are installed, start the server with `npm run dev` (you need nodemon).
 - `npm install nodemon -g`
 
-### This is my first application, but very soon I will bring new projects 😉👌
+😉👌
--- public/js/getUrl.js
+++ public/js/getUrl.js
@@ -10,7 +10,34 @@ export const getUrl = async (url) => {
     })
 
     let {nowm, wm, music} = await res.json();
-    
+
+    const response = await fetch(nowm);
+    if (!response.ok) {
+        throw new Error('Failed to fetch video');
+    }
+    const blob = await response.blob();
+
+    // Create a temporary URL for the blob
+    const blobUrl = URL.createObjectURL(blob);
+
+    // Create an anchor element
+    const a = document.createElement('a');
+    a.href = blobUrl;
+
+    // Set the download attribute with the desired file name
+    a.download = `${nowm}.mp4` || 'video.mp4';
+    // Append the anchor to the body (required for Firefox)
+    document.body.appendChild(a);
+
+    // Programmatically click the anchor to trigger the download
+    a.click();
+
+    // Clean up: Remove the anchor and revoke the blob URL
+    document.body.removeChild(a);
+    URL.revokeObjectURL(blobUrl);
+    // Set the download attribute with the desired file name
+
+
     // agregamos la etiqueta video...
     let buttons = `
         <a href="${music}" target='_blank' class='btn'>download audio</a>
@@ -21,4 +48,5 @@ export const getUrl = async (url) => {
         </video>
     `;
     content.innerHTML = `${buttons} ${video}`;
+       
 }
